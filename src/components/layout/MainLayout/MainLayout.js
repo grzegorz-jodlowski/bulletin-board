d99618576d8be5da6cdc0dd5f375780bc46ddc97
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-23 14:45:43 +0200

    Setup styles for component template

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+import styles from './MainLayout.module.scss';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
 
-const Component = ({ children }) => (
-  <div>
+const Component = ({ className, children }) => (
+  <div className={clsx(className, styles.root)}>
+    <h2>MainLayout</h2>
     {children}
   </div>
 );
 
 Component.propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
 };
 
 // const mapStateToProps = state => ({
