5a8739800aa0e88f665de0a6c22fac5fa514c8b9
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-26 18:57:29 +0200

    Extract card layout to new component Card

---
+++ src/components/common/Card/Card.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+import styles from './Card.module.scss';
+
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+// import { connect } from 'react-redux';
+// import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
+
+const Component = ({ post }) => {
+  const classes = useStyles();
+
+  const { title, image, imageTitle, description, id } = post;
+
+  return (
+    <Link href={`${process.env.PUBLIC_URL}/post/${id}`}>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.cardMedia}
+          image={image}
+          title={imageTitle}
+        />
+        <CardContent className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {title}
+          </Typography>
+          <Typography>
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          {/* <Button size="small" color="primary" href={`${process.env.PUBLIC_URL}/post/${id}`}>
+                    View
+                  </Button> */}
+          {/* <Button size="small" color="primary">
+                    Edit
+                  </Button> */}
+        </CardActions>
+      </Card>
+    </Link>
+  );
+};
+Component.propTypes = {
+  className: PropTypes.string,
+  post: PropTypes.object,
+};
+
+// const mapStateToProps = state => ({
+//   concerts: reduxSelector(state),
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   someAction: arg => dispatch(reduxActionCreator(arg)),
+// });
+
+// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+
+export {
+  Component as Card,
+  // Container as Card,
+  Component as CardComponent, //for tests
+};
+
--- src/components/layout/Cards/Cards.js
+++ src/components/layout/Cards/Cards.js
@@ -4,16 +4,11 @@ import clsx from 'clsx';
 
 import styles from './Cards.module.scss';
 
+import { Card } from '../../common/Card/Card';
+
 import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import Link from '@material-ui/core/Link';
 
 import { connect } from 'react-redux';
 import { getAll } from '../../../redux/postsRedux';
@@ -23,53 +18,18 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
   },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
 }));
 
-const Component = ({ className, children, posts }) => {
+const Component = ({ className, posts }) => {
   const classes = useStyles();
 
   return (
     <div className={clsx(className, styles.root)}>
       <Container className={classes.cardGrid} maxWidth="md">
         <Grid container spacing={4}>
-          {posts.map(({ title, image, imageTitle, description, id }) => (
-            <Grid item key={title} xs={12} sm={6} md={4}>
-              <Link href={`${process.env.PUBLIC_URL}/post/${id}`}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image={image}
-                    title={imageTitle}
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {title}
-                    </Typography>
-                    <Typography>
-                      {description}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    {/* <Button size="small" color="primary" href={`${process.env.PUBLIC_URL}/post/${id}`}>
-                    View
-                  </Button> */}
-                    {/* <Button size="small" color="primary">
-                    Edit
-                  </Button> */}
-                  </CardActions>
-                </Card>
-              </Link>
+          {posts.map((post) => (
+            <Grid item key={post.title} xs={12} sm={6} md={4}>
+              <Card post={post} />
             </Grid>
           ))}
         </Grid>
@@ -79,7 +39,6 @@ const Component = ({ className, children, posts }) => {
 };
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
   posts: PropTypes.array,
 };
