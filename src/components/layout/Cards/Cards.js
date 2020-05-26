c5f069081af3eec082ab81276f9536aaab08af41
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-26 18:37:19 +0200

    Refactor Cards to use state to render cards

--- src/components/layout/Cards/Cards.js
+++ src/components/layout/Cards/Cards.js
@@ -13,9 +13,10 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+import Link from '@material-ui/core/Link';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
+import { connect } from 'react-redux';
+import { getAll } from '../../../redux/postsRedux';
 
 const useStyles = makeStyles((theme) => ({
   cardGrid: {
@@ -35,40 +36,40 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const posts = [1, 2, 3, 4, 5, 6];
-
-const Component = ({ className, children }) => {
+const Component = ({ className, children, posts }) => {
   const classes = useStyles();
 
   return (
     <div className={clsx(className, styles.root)}>
       <Container className={classes.cardGrid} maxWidth="md">
         <Grid container spacing={4}>
-          {posts.map((post) => (
-            <Grid item key={post} xs={12} sm={6} md={4}>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image="https://source.unsplash.com/random"
-                  title="Image title"
-                />
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
-                  </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe the content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small" color="primary">
+          {posts.map(({ title, image, imageTitle, description, id }) => (
+            <Grid item key={title} xs={12} sm={6} md={4}>
+              <Link href={`${process.env.PUBLIC_URL}/post/${id}`}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={image}
+                    title={imageTitle}
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {title}
+                    </Typography>
+                    <Typography>
+                      {description}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    {/* <Button size="small" color="primary" href={`${process.env.PUBLIC_URL}/post/${id}`}>
                     View
-                  </Button>
-                  <Button size="small" color="primary">
+                  </Button> */}
+                    {/* <Button size="small" color="primary">
                     Edit
-                  </Button>
-                </CardActions>
-              </Card>
+                  </Button> */}
+                  </CardActions>
+                </Card>
+              </Link>
             </Grid>
           ))}
         </Grid>
@@ -80,21 +81,22 @@ const Component = ({ className, children }) => {
 Component.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  posts: PropTypes.array,
 };
 
-// const mapStateToProps = state => ({
-//   concerts: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  posts: getAll(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const ReduxContainer = connect(mapStateToProps)(Component);
 
 export {
-  Component as Cards,
-  // Container as Cards,
+  // Component as Cards,
+  ReduxContainer as Cards,
   Component as CardsComponent, //for tests
 };
 
