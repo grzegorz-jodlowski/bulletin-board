9509ac5853d29a1d962a807a7b185d9a53831bfa
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-29 17:51:00 +0200

    Add logout action to Logout button in app bar

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -9,16 +9,22 @@ import Button from '@material-ui/core/Button';
 import styles from './PageNav.module.scss';
 
 import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
+import { updateLoginStatus } from '../../../redux/loginRedux';
+import { updateAdminStatus } from '../../../redux/userRedux';
 
-const Component = ({ className, isLogged }) => (
+const Component = ({ className, isLogged, updateLoginStatus, updateAdminStatus }) => (
   <div className={clsx(className, styles.root)}>
     <Button className={clsx(styles.link)} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'><i className={clsx(styles.logo, 'fas fa-bullhorn')}></i>Announce It!</Button>
     <div>
       <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Homepage</Button>
       {isLogged && <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/posts`} activeClassName='active'>My posts</Button>}
       {isLogged
-        ? <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Log Out</Button>
+        ? <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/`} activeClassName='active' onClick={() => {
+          updateLoginStatus('logout');
+          updateAdminStatus(false);
+        }}
+        >
+          Log Out</Button>
         : <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Log In</Button>}
     </div>
   </div>
@@ -27,17 +33,20 @@ const Component = ({ className, isLogged }) => (
 Component.propTypes = {
   isLogged: PropTypes.bool,
   className: PropTypes.string,
+  updateLoginStatus: PropTypes.func,
+  updateAdminStatus: PropTypes.func,
 };
 
 const mapStateToProps = state => ({
   isLogged: state.isLogged,
 });
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+  updateLoginStatus: log => dispatch(updateLoginStatus(log)),
+  updateAdminStatus: log => dispatch(updateAdminStatus(log)),
+});
 
-const Container = connect(mapStateToProps)(Component);
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
   // Component as PageNav,
