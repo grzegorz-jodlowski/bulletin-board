97288d2072a0f5668262b784d64b753afd477f31
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-01 18:59:06 +0200

    Add fetch data from API to Cards component

--- src/components/layout/Cards/Cards.js
+++ src/components/layout/Cards/Cards.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
+
+import styles from './Cards.module.scss';
 
 import { Card } from '../../common/Card/Card';
 
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
 
 import { connect } from 'react-redux';
-import { getAll } from '../../../redux/postsRedux';
+import { getAll, fetchPublished } from '../../../redux/postsRedux';
 
-const useStyles = makeStyles((theme) => ({
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-}));
+class Component extends React.Component {
 
-const Component = ({ className, posts }) => {
-  const classes = useStyles();
+  componentDidMount() {
+    const { fetchPublishedPosts } = this.props;
 
-  return (
-    <div className={clsx(className)}>
-      <Container className={classes.cardGrid} maxWidth="md">
+    fetchPublishedPosts();
+  }
+
+  render() {
+    const { posts } = this.props;
+
+    return (
+      <Container className={styles.cardGrid} maxWidth="md">
         <Grid container spacing={4}>
           {posts.map((post) => (
             <Grid item key={post.id} xs={12} sm={6} md={4}>
@@ -32,24 +32,24 @@ const Component = ({ className, posts }) => {
           ))}
         </Grid>
       </Container>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Component.propTypes = {
-  className: PropTypes.string,
   posts: PropTypes.array,
+  fetchPublishedPosts: PropTypes.func,
 };
 
 const mapStateToProps = state => ({
   posts: getAll(state),
 });
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+  fetchPublishedPosts: () => dispatch(fetchPublished()),
+});
 
-const ReduxContainer = connect(mapStateToProps)(Component);
+const ReduxContainer = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
   // Component as Cards,
