c82f8aae484bba9f34d62a8ff4ea69fa1502f7f3
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-01 21:45:15 +0200

    Add thunk fetchPostDetails to get currentPost

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 
 /* selectors */
 export const getAll = ({ posts }) => posts.data;
-// export const getAllPublished = ({ posts }) => posts.data.filter(item => item.status == 'published');
+export const getCurrentPost = ({ posts }) => posts.currentPost;
 export const getLoadingState = ({ posts }) => posts.loading;
 
 /* action name creator */
@@ -13,12 +13,14 @@ const createActionName = name => `app/${reducerName}/${name}`;
 /* action types */
 const FETCH_START = createActionName('FETCH_START');
 const FETCH_SUCCESS = createActionName('FETCH_SUCCESS');
+const FETCH_POST_SUCCESS = createActionName('FETCH_POST_SUCCESS');
 const FETCH_ERROR = createActionName('FETCH_ERROR');
 const UPDATE_POST_STATUS = createActionName('UPDATE_POST_STATUS');
 
 /* action creators */
 export const fetchStarted = payload => ({ payload, type: FETCH_START });
 export const fetchSuccess = payload => ({ payload, type: FETCH_SUCCESS });
+export const fetchPostSuccess = payload => ({ payload, type: FETCH_POST_SUCCESS });
 export const fetchError = payload => ({ payload, type: FETCH_ERROR });
 export const updatePostStatus = payload => ({ payload, type: UPDATE_POST_STATUS });
 
@@ -37,6 +39,21 @@ export const fetchPublished = () => {
           dispatch(fetchError(err.message || true));
         });
     }
+  };
+};
+
+export const fetchPostDetails = id => {
+  return (dispatch, getState) => {
+    dispatch(fetchStarted());
+
+    Axios
+      .get(`http://localhost:8000/api/posts/${id}`)
+      .then(res => {
+        dispatch(fetchPostSuccess(res.data));
+      })
+      .catch(err => {
+        dispatch(fetchError(err.message || true));
+      });
   };
 };
 
@@ -73,6 +90,16 @@ export default function reducer(statePart = [], action = {}) {
           error: false,
         },
         data: action.payload,
+      };
+    }
+    case FETCH_POST_SUCCESS: {
+      return {
+        ...statePart,
+        loading: {
+          active: false,
+          error: false,
+        },
+        currentPost: action.payload,
       };
     }
     case FETCH_ERROR: {
