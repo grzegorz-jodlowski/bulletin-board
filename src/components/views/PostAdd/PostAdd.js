07aba363fbc463a01c56bbaf1e7fc89994768170
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-02 16:49:16 +0200

    Refactor PostAdd comp to class

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -16,85 +16,90 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 
 
-const Component = ({ isLogged }) => {
+class Component extends React.Component {
 
-  if (isLogged) {
-    return (
-      <main className={styles.layout}>
-        <Paper className={styles.paper}>
-          <Typography className={styles.header} component="h1" variant="h4" align="center">
-            Add your new ad
-          </Typography>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <TextField
-                required
-                id="author"
-                name="author"
-                label="Email address"
-                fullWidth
-                autoComplete="email"
-              />
+
+  render() {
+    const { isLogged } = this.props;
+
+    if (isLogged) {
+      return (
+        <main className={styles.layout}>
+          <Paper className={styles.paper}>
+            <Typography className={styles.header} component="h1" variant="h4" align="center">
+              Add your new ad
+            </Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id="author"
+                  name="author"
+                  label="Email address"
+                  fullWidth
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id="title"
+                  name="title"
+                  label="Title"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id="text"
+                  name="text"
+                  label="Description"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="phone"
+                  name="phone"
+                  label="Phone"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="price"
+                  name="price"
+                  label="Price"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="photo"
+                  name="photo"
+                  label="Image link"
+                  fullWidth
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                id="title"
-                name="title"
-                label="Title"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                id="text"
-                name="text"
-                label="Description"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                id="phone"
-                name="phone"
-                label="Phone"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                id="price"
-                name="price"
-                label="Price"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                id="photo"
-                name="photo"
-                label="Image link"
-                fullWidth
-              />
-            </Grid>
-          </Grid>
-          <div className={styles.buttons}>
-            <Button
-              variant="contained"
-              color="primary"
-              className={styles.button}
-            >
-              Add
-            </Button>
-          </div>
-        </Paper>
-      </main>
-    );
-  } else {
-    return <Login />;
+            <div className={styles.buttons}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={styles.button}
+              >
+                Add
+              </Button>
+            </div>
+          </Paper>
+        </main>
+      );
+    } else {
+      return <Login />;
+    }
   }
-};
+}
 
 Component.propTypes = {
   children: PropTypes.node,
