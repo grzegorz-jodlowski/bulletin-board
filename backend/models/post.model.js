8b0ca864a3831375ccd2c8ea5896012a2ce4182e
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-02 19:05:58 +0200

    Add server-side validation for post schema

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  author: { type: String, required: true },
-  created: { type: Date, required: true },
-  updated: { type: Date, required: true },
-  status: { type: String, required: true },
-  title: { type: String, required: true },
-  text: { type: String, required: true },
+  author: {
+    type: String,
+    required: [true, 'Email required'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+  },
+  created: { type: Date, required: [true, 'Create date required'] },
+  updated: { type: Date, required: [true, 'Update date required'] },
+  status: { type: String, required: [true, 'Status required'], enum: ['draft', 'published', 'closed'] },
+  title: { type: String, required: [true, 'Title required'], minlength: 10 },
+  text: { type: String, required: [true, 'Description required'], minlength: 20 },
   photo: { type: String },
   price: { type: Number },
   phone: { type: String },
