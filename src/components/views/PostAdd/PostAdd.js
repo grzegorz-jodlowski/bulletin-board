30c4762dd46354a1c4f4502ea3642ea28c31d8db
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-02 16:47:16 +0200

    Refactor PostAdd styles to seperate file

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 //import clsx from 'clsx';
 
-//import styles from './PostAdd.module.scss';
+import styles from './PostAdd.module.scss';
 
 import { connect } from 'react-redux';
 import { getLoginState } from '../../../redux/loginRedux';
@@ -10,58 +10,19 @@ import { getLoginState } from '../../../redux/loginRedux';
 import { Login } from '../Login/Login';
 
 import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 
 
-const useStyles = makeStyles((theme) => ({
-  header: {
-    marginBottom: '25px',
-  },
-  layout: {
-    width: 'auto',
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up(800 + theme.spacing(2) * 2)]: {
-      width: 800,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      padding: theme.spacing(3),
-    },
-  },
-  buttons: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
-  button: {
-    marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(1),
-    paddingLeft: theme.spacing(4),
-    paddingRight: theme.spacing(4),
-
-  },
-}));
-
 const Component = ({ isLogged }) => {
-  const classes = useStyles();
 
   if (isLogged) {
     return (
-      <main className={classes.layout}>
-        <Paper className={classes.paper}>
-          <Typography className={classes.header} component="h1" variant="h4" align="center">
+      <main className={styles.layout}>
+        <Paper className={styles.paper}>
+          <Typography className={styles.header} component="h1" variant="h4" align="center">
             Add your new ad
           </Typography>
           <Grid container spacing={3}>
@@ -118,11 +79,11 @@ const Component = ({ isLogged }) => {
               />
             </Grid>
           </Grid>
-          <div className={classes.buttons}>
+          <div className={styles.buttons}>
             <Button
               variant="contained"
               color="primary"
-              className={classes.button}
+              className={styles.button}
             >
               Add
             </Button>
