44d3aaddc5c04f3b677e13e37cbfb9f707f391fb
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-04 21:31:37 +0200

    Clear App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { createMuiTheme, StylesProvider, ThemeProvider } from '@material-ui/core/styles';
